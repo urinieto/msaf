2e136137df6481132f89c4c63a2921987ee4ac73
Uri Nieto <oriol.nieto@gmail.com>
2014-03-06 05:34:24 -0500

    Working on C++ program to run Levy algorithm

--- algorithms/levy/dsp/segmentation/main.cpp
+++ algorithms/levy/dsp/segmentation/main.cpp
@@ -1,10 +1,37 @@
+/*
+Little program to run the segmenter using the MSAF features
+
+__author__ = "Oriol Nieto"
+__copyright__ = "Copyright 2014, Music and Audio Research Lab (MARL)"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "oriol@nyu.edu"
+*/
+
 #include "ClusterMeltSegmenter.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World\n");
+    printf("Initialising...\n");
+    ClusterMeltSegmenterParams params; // use default params
+    ClusterMeltSegmenter *segmenter = new ClusterMeltSegmenter(params);
+
+    // Read features from JAMS
+    vector<vector<double> > f;
+
+    // Set features
+    segmenter->setFeatures(f);
+
+    // Segment
+    segmenter->segment();
+
+    // Clean up
+    delete segmenter;
+
+    // Done
     return 0;
 }
