99a61b1569f98089d8c38b80d9312425df11ec6d
Uri Nieto <oriol.nieto@gmail.com>
2014-03-06 09:54:40 -0500

    Reading JSON files in C++ with JsonCpp
    Former-commit-id: 69d9597346370124cd6fc0292ce91ea2f72fa89d
    Former-commit-id: 6bbda087bcec72f034c63d11401880d6da105c84

--- algorithms/levy/main.cpp
+++ algorithms/levy/main.cpp
@@ -10,15 +10,43 @@ __email__ = "oriol@nyu.edu"
 
 #include "ClusterMeltSegmenter.h"
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <json/json.h>
 
 using namespace std;
+
+int readJSON(const char* json_path) {
+
+    std::filebuf fb;
+    fb.open(json_path, ios::in);
+    istream inputs(&fb);
+
+    Json::Value root;   // will contains the root value after parsing.
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse( inputs, root );
+    if ( !parsingSuccessful )
+    {
+        // report to the user the failure and their locations in the document.
+        std::cout  << "Failed to parse configuration\n"
+                   << reader.getFormattedErrorMessages();
+        return 1;
+    }
+
+    cout << root << endl;
+    fb.close();
+
+    return 0;
+}
 
 int main(int argc, char const *argv[])
 {
     printf("Initialising...\n");
     ClusterMeltSegmenterParams params; // use default params
     ClusterMeltSegmenter *segmenter = new ClusterMeltSegmenter(params);
+
+    // Open JSON
+    readJSON("/Users/uri/datasets/Segments/estimations/Isophonics_01_-_No_Reply.json");
 
     // Read features from JAMS
     vector<vector<double> > f;
