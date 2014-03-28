c02b44b8f566b7b9822742765b6aeb3fb1973fb2
Uri Nieto <oriol.nieto@gmail.com>
2014-03-28 16:32:42 -0400

    Updated to PEP8 syntax
    Former-commit-id: 0c6e28f5a93a6a08b2da1edd1cf189fb6c632443
    Former-commit-id: 04ec00214b80fbe7fcd08eb15eab6ec676b4b229

--- eval_mma.py
+++ eval_mma.py
@@ -12,19 +12,14 @@ __email__       = "oriol@nyu.edu"
 
 import argparse
 import glob
-import json
 import logging
 import os
-import pylab as plt
 import numpy as np
 import time
-import sys
 import sqlite3
 import itertools
 
-import utils
 import mir_eval
-import jams
 
 import msaf_io as MSAF
 
@@ -36,13 +31,16 @@ feat_dict = {
     'olda'  :   ''
 }
 
+
+
 def print_results(results):
     """Print the results."""
     results = np.asarray(results)
     res = results.mean(axis=0)
-    logging.info("F3: %.2f, P3: %2.f, R3: %2.f, F05: %.2f, P05: %.2f, " \
-        "R05: %.2f, D: %.4f" % (100*res[2], 100*res[0], 100*res[1], 100*res[5],
-            100*res[3], 100*res[4], res[6]))
+    logging.info("F3: %.2f, P3: %2.f, R3: %2.f, F05: %.2f, P05: %.2f, "
+                 "R05: %.2f, D: %.4f" % (100 * res[2], 100 * res[0],
+                                         100 * res[1], 100 * res[5],
+                                         100 * res[3], 100 * res[4], res[6]))
 
 
 def save_results_ds(cursor, alg_id, results, annot_beats, trim,
@@ -64,7 +62,7 @@ def save_results_ds(cursor, alg_id, results, annot_beats, trim,
 
     if track_id is not None:
         all_values = (track_id, res[5], res[3], res[4], res[2], res[0], res[1],
-                                res[6], annot_beats, feature, "none", trim)
+                      res[6], annot_beats, feature, "none", trim)
         table = "%s_bounds" % alg_id
         select_where = "track_id=?"
         select_values = (track_id, annot_beats, feature, trim)
@@ -72,14 +70,15 @@ def save_results_ds(cursor, alg_id, results, annot_beats, trim,
         # Aggregate results
         res = np.mean(res, axis=0)
         all_values = (alg_id, ds_name, res[5], res[3], res[4], res[2], res[0],
-                        res[1], res[6], annot_beats, feature, "none", trim)
+                      res[1], res[6], annot_beats, feature, "none", trim)
         table = "boundaries"
         select_where = "algo_id=? AND ds_name=?"
         select_values = (alg_id, ds_name, annot_beats, feature, trim)
 
     # Check if exists
-    cursor.execute("SELECT * FROM %s WHERE %s AND annot_beat=? AND " \
-            "feature=? AND trim=?"% (table, select_where), select_values)
+    cursor.execute("SELECT * FROM %s WHERE %s AND annot_beat=? AND "
+                   "feature=? AND trim=?" % (table, select_where),
+                   select_values)
 
     # Insert new if it doesn't exist
     if cursor.fetchone() is None:
@@ -89,13 +88,12 @@ def save_results_ds(cursor, alg_id, results, annot_beats, trim,
     else:
         # Update row
         evaluations = (res[5], res[3], res[4], res[2], res[0],
-                        res[1], res[6])
+                       res[1], res[6])
         evaluations += select_values
         sql_cmd = "UPDATE %s SET F05=?, P05=?, R05=?, F3=?, " \
-                    "P3=?, R3=?, D=?  WHERE %s AND annot_beat=? AND " \
-                    "feature=? AND trim=?" % (table, select_where)
+                  "P3=?, R3=?, D=?  WHERE %s AND annot_beat=? AND " \
+                  "feature=? AND trim=?" % (table, select_where)
         cursor.execute(sql_cmd, evaluations)
-
 
 
 def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
@@ -110,16 +108,15 @@ def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
     # Get estimation files (each one contains the results of all the
     # algorithms)
     est_files = glob.glob(os.path.join(in_path, "estimations",
-                                            "%s_*.json" % ds_name))
-
+                                       "%s_*.json" % ds_name))
     conn = sqlite3.connect("results/results.sqlite")
-    conn.text_factory = str # Fixes the 8-bit encoding string problem
+    conn.text_factory = str     # Fixes the 8-bit encoding string problem
     c = conn.cursor()
 
     logging.info("Evaluating %d tracks..." % len(est_files))
 
     # Compute features for each file
-    results = np.empty((0,7))   # Results: P3, R3, F3, P05, R05, F05, D
+    results = np.empty((0, 7))   # Results: P3, R3, F3, P05, R05, F05, D
 
     # Dataset results
     results_ds = []
@@ -134,8 +131,6 @@ def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
 
     for est_file in est_files:
 
-        ds_prefix = os.path.basename(est_file).split("_")[0]
-
         if salamii:
             num = int(os.path.basename(est_file).split("_")[1].split(".")[0])
             if num < 956 or num > 1498:
@@ -143,13 +138,15 @@ def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
 
         # Get all the permutations in order to compute the MMA
         results_mma = []
-        for algorithms in itertools.combinations(MSAF.get_algo_ids(est_file),2):
+        for algorithms in itertools.combinations(MSAF.get_algo_ids(est_file),
+                                                 2):
             # Read estimated times from both algorithms
             est_times1 = MSAF.read_boundaries(est_file, algorithms[0],
                                 annot_beats, feature=feat_dict[algorithms[0]])
             est_times2 = MSAF.read_boundaries(est_file, algorithms[1],
                                 annot_beats, feature=feat_dict[algorithms[1]])
-            if est_times1 == [] or est_times2 == []: continue
+            if est_times1 == [] or est_times2 == []:
+                continue
 
             # F-measures
             P3, R3, F3 = mir_eval.segment.boundary_detection(est_times1,
@@ -160,10 +157,10 @@ def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
             # Information gain
             try:
                 D = mir_eval.beat.information_gain(est_times1,
-                                                est_times2, bins=10)
+                                                   est_times2, bins=10)
             except:
-                logging.warning("Couldn't compute the Information Gain for " \
-                                                        "file %s" % est_file)
+                logging.warning("Couldn't compute the Information Gain for "
+                                "file %s" % est_file)
                 D = 0
             # Store partial results
             results_mma.append([P3, R3, F3, P05, R05, F05, D])
@@ -172,14 +169,14 @@ def process(in_path, ds_name="*", annot_beats=False, trim=False, **params):
         results_ds.append(np.mean(np.asarray(results_mma), axis=0))
 
         # Save Track Result to database
-        save_results_ds(c, alg_id, results_ds[-1],
-            annot_beats, trim, feature, track_id=os.path.basename(est_file))
+        save_results_ds(c, alg_id, results_ds[-1], annot_beats, trim, feature,
+                        track_id=os.path.basename(est_file))
 
         # Store dataset results if needed
         actual_ds_name = os.path.basename(est_file).split("_")[0]
         if curr_ds != actual_ds_name:
-            save_results_ds(c, alg_id, results_ds, annot_beats,
-                    trim, feature, ds_name=curr_ds)
+            save_results_ds(c, alg_id, results_ds, annot_beats, trim, feature,
+                            ds_name=curr_ds)
             curr_ds = actual_ds_name
             # Add to global results
             results = np.concatenate((results, np.asarray(results_ds)))
@@ -216,8 +213,8 @@ def main():
                         action="store",
                         dest="ds_name",
                         default="*",
-                        help="The prefix of the dataset to use "\
-                            "(e.g. Isophonics, SALAMI")
+                        help="The prefix of the dataset to use "
+                        "(e.g. Isophonics, SALAMI")
     parser.add_argument("-b",
                         action="store_true",
                         dest="annot_beats",
@@ -239,11 +236,11 @@ def main():
 
     # Setup the logger
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
-        level=logging.INFO)
+                        level=logging.INFO)
 
     # Run the algorithm
     process(args.in_path, args.ds_name, args.annot_beats,
-                    trim=args.trim, feature=args.feature)
+            trim=args.trim, feature=args.feature)
 
     # Done!
     logging.info("Done! Took %.2f seconds." % (time.time() - start_time))
