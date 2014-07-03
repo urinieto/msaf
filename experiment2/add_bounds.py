d1b9ad9678362fbd87575f6ad904f0578d4a6dcb
Uri Nieto <oriol.nieto@gmail.com>
2014-07-03 15:06:53 -0500

    Working on finding the excerpts and synthesize the 3 different versions for experiment 2
    Former-commit-id: e34826d2275777fa921ca4b225da162989e425a0
    Former-commit-id: f2dad593c14b3ea407932af182ef95255164abec

---
+++ experiment2/add_bounds.py
@@ -0,0 +1,134 @@
+"""
+Adds boundaries to a specific audio file.
+"""
+
+import glob
+import numpy as np
+import os
+from scikits import audiolab
+import subprocess
+
+
+maindir = "dataset"
+outputdir = "www/outputsongs"
+
+
+def ensure_dir(directory):
+    """Checks if directory exists, and creates it otherwise."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def resample_audio():
+    """Resamples the audio from maindir/audio to 44100Hz."""
+    src_mp3s = glob.glob(os.path.join(maindir, "audio", "*.mp3"))
+    for src_mp3 in src_mp3s:
+        dest_wav = src_mp3.replace(".mp3", ".wav")
+        mp32wav(src_mp3, dest_wav)
+
+
+def mp32wav(mp3file, wavfile, sr=44100):
+    """Converts an mp3 to a wav file."""
+    cmd = "sox -S %s -r %d -b 16 %s" % (mp3file, sr, wavfile)
+    subprocess.call(cmd.split(" "))
+
+
+def wav2mp3(wavfile, mp3file, bitrate=128):
+    """Converts a wav file to an mp3 file."""
+    cmd = "sox -c 1 %s -C %d %s" % (wavfile, bitrate, mp3file)
+    subprocess.call(cmd.split(" "))
+
+
+def read_wav(wavfile):
+    """Reads the wav file and downsamples to 11025 Hz if needed.
+
+        @param wavfile string: Path to the wave file.
+        @return x np.array: Array of samples of the audio file.
+        @return fs int: Sampling frequency.
+    """
+    assert os.path.isfile(wavfile), \
+        'ERROR: wivefile file %s does not exist' % wavfile
+
+    x, fs, enc = audiolab.wavread(wavfile)
+    if len(x.shape) >= 2:
+        x = x[:, 0]  # Make mono
+
+    assert fs == 44100, \
+        "ERROR: File %s is not sampled at 44100 Hz" % wavfile
+
+    return x, fs
+
+
+def read_gt_boundaries(labfile):
+
+    """Reads the boundaries from a lab file and puts them in a numpy array."""
+    f = open(labfile, "r")
+    lines = f.readlines()
+    f.close()
+
+    bounds = []
+    for line in lines:
+        bound = line.split(" ")[0].split("\t")[0]
+        bounds.append(float(bound))
+    last_bound = lines[-1].split(" ")[0].split("\t")[0]
+    bounds.append(float(last_bound))
+
+    return np.asarray(bounds)
+
+
+def add_boundaries(wavfile, boundaries, output='output.wav',
+                   boundsound="sounds/bell.wav", start=0, end=None):
+    """Adds a cowbell sound for each boundary and saves it into a new wav file.
+
+        @param wavfile string: Input wav file (sampled at 11025Hz or 44100Hz).
+        @param boundaries np.array: Set of times representing the boundaries
+            (in seconds).
+        @param output string: Name of the output wav file.
+        @param boundsound string: Sound to add to the original file.
+        @param start float: Start time (in seconds)
+        @param end float: End time (in seconds)
+
+    """
+
+    OFFSET = 0.0  # offset time in seconds
+
+    x, fs = read_wav(wavfile)
+    xb, fsb = read_wav(boundsound)
+
+    # Normalize
+    x /= x.max()
+
+    # Copy the input wav file to the output
+    out = np.zeros(x.size + xb.size + 1000)
+    out[:x.size] = x / 3.0
+
+    # Add boundaries
+    for bound in boundaries:
+        start_idx = int((bound + OFFSET) * fs)
+        end_idx = start_idx + xb.size
+        read_frames = out[start_idx:end_idx].size
+        out[start_idx:end_idx] += xb[:read_frames]
+
+    # Cut track adding some extra time
+    extra_time = 4 * fs
+    start_time = start * fs - extra_time
+    if start_time < 0:
+        start_time = 0
+    if end is None:
+        end_time = len(out)
+    else:
+        end_time = end * fs + extra_time
+        if end_time > len(out):
+            end_time = len(out)
+
+    out = out[start_time:end_time]
+
+    # Write output wav
+    audiolab.wavwrite(out, output, fs=fs)
+
+    # Convert to MP3 and delete wav
+    dest_mp3 = output.replace(".wav", ".mp3").replace("##_", "")
+    wav2mp3(output, dest_mp3)
+    os.remove(output)
+
+    print "Wrote %s" % dest_mp3
