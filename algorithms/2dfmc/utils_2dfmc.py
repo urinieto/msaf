fb0fa67ef3e34d2de3426451539bf1fbc172c1f2
Uri Nieto <oriol.nieto@gmail.com>
2014-08-02 13:48:21 -0400

    2D-FMC working

---
+++ algorithms/2dfmc/utils_2dfmc.py
@@ -0,0 +1,130 @@
+"""
+Set of util functions for the section similarity project.
+"""
+
+import copy
+import numpy as np
+import json
+import scipy.fftpack
+import pylab as plt
+
+def resample_mx(X, incolpos, outcolpos):
+    """
+    Y = resample_mx(X, incolpos, outcolpos)
+    X is taken as a set of columns, each starting at 'time'
+    colpos, and continuing until the start of the next column.
+    Y is a similar matrix, with time boundaries defined by
+    outcolpos.  Each column of Y is a duration-weighted average of
+    the overlapping columns of X.
+    2010-04-14 Dan Ellis dpwe@ee.columbia.edu  based on samplemx/beatavg
+    -> python: TBM, 2011-11-05, TESTED
+    """
+    noutcols = len(outcolpos)
+    Y = np.zeros((X.shape[0], noutcols))
+    # assign 'end times' to final columns
+    if outcolpos.max() > incolpos.max():
+        incolpos = np.concatenate([incolpos,[outcolpos.max()]])
+        X = np.concatenate([X, X[:,-1].reshape(X.shape[0],1)], axis=1)
+    outcolpos = np.concatenate([outcolpos, [outcolpos[-1]]])
+    # durations (default weights) of input columns)
+    incoldurs = np.concatenate([np.diff(incolpos), [1]])
+
+    for c in range(noutcols):
+        firstincol = np.where(incolpos <= outcolpos[c])[0][-1]
+        firstincolnext = np.where(incolpos < outcolpos[c+1])[0][-1]
+        lastincol = max(firstincol,firstincolnext)
+        # default weights
+        wts = copy.deepcopy(incoldurs[firstincol:lastincol+1])
+        # now fix up by partial overlap at ends
+        if len(wts) > 1:
+            wts[0] = wts[0] - (outcolpos[c] - incolpos[firstincol])
+            wts[-1] = wts[-1] - (incolpos[lastincol+1] - outcolpos[c+1])
+        wts = wts * 1. /sum(wts)
+        Y[:,c] = np.dot(X[:,firstincol:lastincol+1], wts)
+    # done
+    return Y
+
+def magnitude(X):
+    """Magnitude of a complex matrix."""
+    r = np.real(X)
+    i = np.imag(X)
+    return np.sqrt(r * r + i * i);
+
+def json_to_bounds(segments_json):
+    """Extracts the boundaries from a json file and puts them into 
+        an np array."""
+    f = open(segments_json)
+    segments = json.load(f)["segments"]
+    bounds = []
+    for segment in segments:
+        bounds.append(segment["start"])
+    bounds.append(bounds[-1] + segments[-1]["duration"]) # Add last boundary
+    f.close()
+    return np.asarray(bounds)
+
+def json_bounds_to_bounds(bounds_json):
+    """Extracts the boundaries from a bounds json file and puts them into 
+        an np array."""
+    f = open(bounds_json)
+    segments = json.load(f)["bounds"]
+    bounds = []
+    for segment in segments:
+        bounds.append(segment["start"])
+    f.close()
+    return np.asarray(bounds)
+
+def json_to_labels(segments_json):
+    """Extracts the labels from a json file and puts them into 
+        an np array."""
+    f = open(segments_json)
+    segments = json.load(f)["segments"]
+    labels = []
+    str_labels = []
+    for segment in segments:
+        if not segment["label"] in str_labels:
+            str_labels.append(segment["label"])
+            labels.append(len(str_labels)-1)
+        else:
+            label_idx = np.where(np.asarray(str_labels) == segment["label"])[0][0]
+            labels.append(label_idx)
+    f.close()
+    return np.asarray(labels)
+
+def json_to_beats(beats_json_file):
+    """Extracts the beats from the beats_json_file and puts them into
+        an np array."""
+    f = open(beats_json_file, "r") 
+    beats_json = json.load(f)
+    beats = []
+    for beat in beats_json["beats"]:
+        beats.append(beat["start"])
+    f.close()
+    return np.asarray(beats)
+
+def analyze_results(file):
+    f = open(file, "r")
+    lines = f.readlines()
+    F = []
+    for line in lines:
+        F.append(float(line.split("\t")[0]))
+    f.close()
+    print np.mean(F)
+
+def compute_ffmc2d(X):
+    """Computes the 2D-Fourier Magnitude Coefficients."""
+    # 2d-fft
+    fft2 = scipy.fftpack.fft2(X)
+
+    # Magnitude
+    fft2m = magnitude(fft2)
+
+    # FFTshift and flatten
+    fftshift = scipy.fftpack.fftshift(fft2m).flatten()
+
+    #cmap = plt.cm.get_cmap('hot')
+    #plt.imshow(np.log1p(scipy.fftpack.fftshift(fft2m)).T, interpolation="nearest", 
+    #    aspect="auto", cmap=cmap)
+    #plt.show()
+
+    # Take out redundant components
+    return fftshift[:fftshift.shape[0]/2+1]
---
+++ algorithms/2dfmc/xmeans.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+"""Class that implements X-means."""
+
+import argparse
+import numpy as np
+import time
+import pylab as plt
+import scipy.cluster.vq as vq
+from scipy.spatial import distance
+import sys
+
+class XMeans:
+    def __init__(self, X, init_K=2, plot=False):
+        self.X = X
+        self.init_K = init_K
+        self.plot = plot
+
+    def estimate_K_xmeans(self, th=0.2):
+        """Estimates K running X-means algorithm (Pelleg & Moore, 2000)."""
+
+        # Run initial K-means
+        means, labels = self.run_kmeans(self.X, self.init_K)
+
+        # Run X-means algorithm
+        stop = False
+        curr_K = self.init_K
+        while not stop:
+            stop = True
+            final_means = []
+            for k in xrange(curr_K):
+                # Find the data that corresponds to the k-th cluster
+                D = self.get_clustered_data(self.X, labels, k)
+                if len(D) == 0: continue
+
+                # Whiten and find whitened mean
+                stdD = np.std(D,axis=0)
+                #D = vq.whiten(D)
+                D /= stdD # Same as line above
+                mean = D.mean(axis=0)
+
+                # Cluster this subspace by half (K=2)
+                half_means, half_labels = self.run_kmeans(D, K=2)
+
+                # Get subclusters
+                D_half1 = self.get_clustered_data(D, half_labels, 0)
+                D_half2 = self.get_clustered_data(D, half_labels, 1)
+
+                # Compute BICs
+                bic1 = self.compute_bic(D, [mean], K=1, labels=np.zeros(D.shape[0]), R=D.shape[0])
+                bic2 = self.compute_bic(D, half_means, K=2, labels=half_labels, R=D.shape[0])
+
+                # Split or not
+                max_bic = np.max([np.abs(bic1), np.abs(bic2)])
+                norm_bic1 = bic1/max_bic
+                norm_bic2 = bic2/max_bic
+                diff_bic = np.abs(norm_bic1 - norm_bic2)
+                # Split!
+                if diff_bic > th:
+                    final_means.append(half_means[0] * stdD)
+                    final_means.append(half_means[1] * stdD)
+                    curr_K += 1
+                    stop = False
+                # Don't split
+                else:
+                    final_means.append(mean * stdD)
+
+            final_means = np.asarray(final_means)
+
+            if self.plot:
+                plt.scatter(self.X[:,0], self.X[:,1])
+                plt.scatter(final_means[:,0], final_means[:,1], color="y")
+                plt.show()
+
+            labels, dist = vq.vq(self.X, final_means)
+
+        return curr_K
+
+    def estimate_K_knee(self, th=.015, maxK=12):
+        """Estimates the K using K-means and BIC, by sweeping various K and 
+            choosing the optimal BIC."""
+        # Sweep K-means
+        if self.X.shape[0] < maxK:
+            maxK = self.X.shape[0]
+        if maxK < 2:
+            maxK = 2
+        K = np.arange(1,maxK)
+        bics = []
+        for k in K:
+            means, labels = self.run_kmeans(self.X, k)
+            bic = self.compute_bic(self.X, means, labels, K=k, R=self.X.shape[0])
+            bics.append(bic)
+        diff_bics = np.diff(bics)
+        print maxK
+        finalK = K[-1]
+
+        if len(bics) == 1:
+            finalK = 2
+        else:
+            # Normalize
+            bics = np.asarray(bics)
+            bics -= bics.min()
+            bics /= bics.max()
+            diff_bics -= diff_bics.min()
+            diff_bics /= diff_bics.max()
+
+            print bics, diff_bics
+
+            # Find optimum K
+            for i in xrange(len(K[:-1])):
+                #if bics[i] > diff_bics[i]:
+                if diff_bics[i] < th:
+                    finalK = K[i]
+                    break
+
+        if self.plot:
+            plt.figure(0)
+            plt.plot(K, bics)
+            plt.plot(K[:-1], diff_bics)
+            plt.figure(1)
+            plt.scatter(self.X[:,0], self.X[:,1])
+            plt.show()
+
+        return finalK
+
+    def get_clustered_data(self, X, labels, label_index):
+        """Returns the data with a specific label_index, using the previously
+         learned labels."""
+        D = X[np.argwhere(labels == label_index)]
+        return D.reshape((D.shape[0], D.shape[-1]))
+
+    def run_kmeans(self, X, K):
+        """Runs k-means and returns the labels assigned to the data."""
+        wX = vq.whiten(X)
+        means, dist = vq.kmeans(wX, K, iter=100)
+        labels, dist = vq.vq(wX, means)
+        return means, labels
+
+    def compute_bic(self, D, means, labels, K, R):
+        """Computes the Bayesian Information Criterion."""
+        D = vq.whiten(D)
+        Rn = D.shape[0]
+        M = D.shape[1]
+
+        if R == K:
+            return 1
+
+        # Maximum likelihood estimate (MLE)
+        mle_var = 0
+        for k in xrange(len(means)):
+            X = D[np.argwhere(labels == k)]
+            X = X.reshape((X.shape[0], X.shape[-1]))
+            for x in X:
+                mle_var += distance.euclidean(x, means[k])
+                #print x, means[k], mle_var
+        mle_var /= (float(R - K))
+
+        # Log-likelihood of the data
+        l_D = - Rn/2. * np.log(2*np.pi) - (Rn * M)/2. * np.log(mle_var) - \
+                (Rn - K)/2. + Rn *  np.log(Rn) - Rn * np.log(R)
+
+        # Params of BIC
+        p = (K-1) + M * K + mle_var
+
+        print l_D, p, R, K
+
+        # Return the bic
+        return l_D - p/2. * np.log(R)
+
+    @classmethod
+    def generate_2d_data(self, N=100, K=5):
+        """Generates N*K 2D data points with K means and N data points
+            for each mean."""
+        # Seed the random
+        np.random.seed(seed=int(time.time()))
+
+        # Amount of spread of the centroids
+        spread = 30
+
+        # Generate random data
+        X = np.empty((0,2))
+        for i in xrange(K):
+            mean = np.array([np.random.random()*spread, 
+                             np.random.random()*spread])
+            x = np.random.normal(0.0, scale=1.0, size=(N, 2)) + mean
+            X = np.append(X, x, axis=0)
+
+        return X
+
+
+def test_kmeans(K=5):
+    """Test k-means with the synthetic data."""
+    X = XMeans.generate_2d_data(K=4)
+    wX = vq.whiten(X)
+    dic, dist = vq.kmeans(wX, K, iter=100)
+
+    plt.scatter(wX[:,0], wX[:,1])
+    plt.scatter(dic[:,0], dic[:,1], color="m")
+    plt.show()
+
+
+
+def main(args):
+    #test_kmeans(6)
+    X = XMeans.generate_2d_data(K=args.k)
+    xmeans = XMeans(X, init_K=2, plot=args.plot)
+    est_K = xmeans.estimate_K_xmeans()
+    est_K_knee = xmeans.estimate_K_knee()
+    print "Estimated x-means K:", est_K
+    print "Estimated Knee Point Detection K:", est_K_knee
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Runs x-means")
+    parser.add_argument("k",
+                        metavar="k", type=int,
+                        help="Number of clusters to estimate.")
+    parser.add_argument("-p", action="store_true", default=False,
+                        dest="plot", help="Plot the results")
+    main(parser.parse_args())
+
+
