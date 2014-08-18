eb3a98e14cd6cdedc018ca29e429450b7bc65e25
Uri Nieto <oriol.nieto@gmail.com>
2014-08-17 22:38:07 -0400

    Added new features and framesync features to get_features
    Former-commit-id: 01d3315bb92ca4b422aa0c8a853ab3e7eaf7096f
    Former-commit-id: eb9469276f89240d3679f397158a4aa3686e5edc

--- input_output.py
+++ input_output.py
@@ -127,75 +127,6 @@ def read_estimations(est_file, boundaries_id, labels_id=None, **params):
     return np.asarray(boundaries), np.asarray(labels, dtype=int)
 
 
-def read_estimations_old(est_file, alg_id, annot_beats, annot_bounds=False,
-                     bounds=True, **params):
-    """Reads the estimations (either boundaries or labels) from an estimated
-    file.
-
-    Parameters
-    ----------
-    est_file: str
-        Path to the estimated file (JSON file).
-    alg_id: str
-        Algorithm ID from which to retrieve the boundaries. E.g. cnmf
-    annot_beats: bool
-        Whether to retrieve the boundaries using annotated beats or not.
-    annot_bounds: bool
-        Whether to retrieve the boundaries using annotated bounds or not.
-    bounds : bool
-        Whether to extract boundaries or labels
-    params: dict
-        Additional search parameters. E.g. {"feature" : "hpcp"}
-
-    Returns
-    -------
-    estimations = np.array(N,2) or np.array(N)
-        Set of read estimations:
-            If boundaries: interval format (start, end).
-            If labels: flattened format.
-    """
-    est_data = json.load(open(est_file, "r"))
-
-    if bounds:
-        est_type = "boundaries"
-    else:
-        est_type = "labels"
-
-    if est_type not in est_data.keys() or alg_id not in est_data[est_type]:
-        if bounds:
-            logging.error("Estimation not found for algorithm %s in %s" %
-                          (est_file, alg_id))
-        return []
-
-    estimations = []
-    for alg in est_data[est_type][alg_id]:
-        if alg["annot_beats"] == annot_beats:
-            # Match non-empty parameters
-            found = True
-            for key in params:
-                if key != "feature" and (key not in alg.keys() or
-                        (params[key] != "" and alg[key] != params[key])):
-                    found = False
-
-            if not bounds:
-                if "annot_bounds" in alg.keys():
-                    if alg["annot_bounds"] != annot_bounds:
-                        found = False
-
-            if found:
-                estimations = np.array(alg["data"])
-                if bounds:
-                    # Sort the boundaries by time
-                    estimations = np.sort(estimations)
-                break
-
-    # Convert to interval if needed
-    if bounds:
-        estimations = np.asarray(zip(estimations[:-1], estimations[1:]))
-
-    return estimations
-
-
 def get_algo_ids(est_file):
     """Gets the algorithm ids that are contained in the est_file."""
     with open(est_file, "r") as f:
@@ -204,68 +135,69 @@ def get_algo_ids(est_file):
     return algo_ids
 
 
-def read_annotations(audio_path):
+def read_references(audio_path):
     """Reads the boundary frames and the labels."""
     # Dataset path
     ds_path = os.path.dirname(os.path.dirname(audio_path))
 
-    # Read annotations
-    jam_path = os.path.join(ds_path, "annotations",
-                            os.path.basename(audio_path)[:-4] + ".jams")
+    # Read references
+    jam_path = os.path.join(ds_path, msaf.Dataset.references_dir,
+                            os.path.basename(audio_path)[:-4] +
+                            msaf.Dataset.references_ext)
     ds_prefix = os.path.basename(audio_path).split("_")[0]
-    ann_inters, ann_labels = jams2.converters.load_jams_range(
+    ref_inters, ref_labels = jams2.converters.load_jams_range(
         jam_path, "sections", context=msaf.prefix_dict[ds_prefix])
     try:
-        ann_inters, ann_labels = jams2.converters.load_jams_range(
+        ref_inters, ref_labels = jams2.converters.load_jams_range(
             jam_path, "sections", context=msaf.prefix_dict[ds_prefix])
     except:
-        logging.warning("Annotation not found in %s" % jam_path)
+        logging.warning("Reference not found in %s" % jam_path)
         return []
 
     # Intervals to times
-    ann_times = np.concatenate((ann_inters.flatten()[::2],
-                                [ann_inters[-1, -1]]))
+    ref_times = np.concatenate((ref_inters.flatten()[::2],
+                                [ref_inters[-1, -1]]))
 
-    return ann_times, ann_labels
+    return ref_times, ref_labels
 
 
-def read_annot_labels(audio_path):
+def read_ref_labels(audio_path):
     """Reads the annotated labels from the given audio path."""
-    ann_times, ann_labels = read_annotations(audio_path)
-    return ann_labels
+    ref_times, ref_labels = read_references(audio_path)
+    return ref_labels
 
 
-def read_annot_int_labels(audio_path):
+def read_ref_int_labels(audio_path):
     """Reads the annotated labels using unique integers as identifiers
     instead of strings."""
-    ann_labels = read_annot_labels(audio_path)
+    ref_labels = read_ref_labels(audio_path)
     labels = []
     label_dict = {}
     k = 1
-    for ann_label in ann_labels:
-        if ann_label in label_dict.keys():
-            labels.append(label_dict[ann_label])
+    for ref_label in ref_labels:
+        if ref_label in label_dict.keys():
+            labels.append(label_dict[ref_label])
         else:
-            label_dict[ann_label] = k
+            label_dict[ref_label] = k
             labels.append(k)
             k += 1
     return labels
 
 
-def read_annot_bound_frames(audio_path, beats):
-    """Reads the corresponding annotations file to retrieve the boundaries
+def read_ref_bound_frames(audio_path, beats):
+    """Reads the corresponding references file to retrieve the boundaries
         in frames."""
 
-    ann_times, ann_labels = read_annotations(audio_path)
+    ref_times, ref_labels = read_references(audio_path)
 
     # align with beats
-    dist = np.minimum.outer(ann_times, beats)
+    dist = np.minimum.outer(ref_times, beats)
     bound_frames = np.argmax(np.maximum(0, dist), axis=1)
 
     return bound_frames
 
 
-def get_features(audio_path, annot_beats=False):
+def get_features(audio_path, annot_beats=False, beatsync=True):
     """
     Gets the features of an audio file given the audio_path.
 
@@ -273,61 +205,66 @@ def get_features(audio_path, annot_beats=False):
     ----------
     audio_path: str
         Path to the audio file.
-
     annot_beats: bool
         Whether to use annotated beats or not.
+    beatsync: bool
+        Whether to use beat-sync features or not.
 
     Return
     ------
-    C: np.array((T, 12))
-        Beat-sync Chromagram
-
-    M: np.array((T, 13))
-        Beat-sync MFCC
-
+    C: np.array((N, 12))
+        (Beat-sync) Chromagram
+    M: np.array((N, 13))
+        (Beat-sync) MFCC
+    T: np.array((N, 6))
+        (Beat-sync) Tonnetz
     beats: np.array(T)
         Beats in seconds
-
     dur: float
         Song duration
-
+    analysis : dict
+        Parameters of analysis of track (e.g. sampling rate)
     """
     # Dataset path
     ds_path = os.path.dirname(os.path.dirname(audio_path))
 
-    # Read annotations
-    annotation_path = os.path.join(ds_path, "annotations",
-        os.path.basename(audio_path)[:-4] + ".jams")
+    # Read references
+    annotation_path = os.path.join(ds_path, msaf.Dataset.references_dir,
+        os.path.basename(audio_path)[:-4] + msaf.Dataset.references_ext)
     jam = jams2.load(annotation_path)
 
     # Read Estimations
-    features_path = os.path.join(ds_path, "features",
-        os.path.basename(audio_path)[:-4] + ".json")
-    f = open(features_path, "r")
-    feats = json.load(f)
+    features_path = os.path.join(ds_path, msaf.Dataset.features_dir,
+        os.path.basename(audio_path)[:-4] + msaf.Dataset.features_ext)
+    with open(features_path, "r") as f:
+        feats = json.load(f)
 
-    # Beat Synchronous Chroma
-    if annot_beats:
-        C = np.asarray(feats["ann_beatsync"]["hpcp"])
-        M = np.asarray(feats["ann_beatsync"]["mfcc"])
-        beats = []
-        beat_data = jam.beats[0].data
-        if beat_data == []:
-            raise ValueError
-        for data in beat_data:
-            beats.append(data.time.value)
-        beats = np.unique(beats)
+    # Beat Synchronous Feats
+    if beatsync:
+        if annot_beats:
+            feat_str = "ann_beatsync"
+            beats = []
+            beat_data = jam.beats[0].data
+            if beat_data == []:
+                raise ValueError
+            for data in beat_data:
+                beats.append(data.time.value)
+            beats = np.unique(beats)
+        else:
+            feat_str = "est_beatsync"
+            beats = np.asarray(feats["beats"]["ticks"])[0]
     else:
-        C = np.asarray(feats["est_beatsync"]["hpcp"])
-        M = np.asarray(feats["est_beatsync"]["mfcc"])
-        beats = np.asarray(feats["beats"]["ticks"])[0]
+        feat_str = "framesync"
+        beats = None
+    C = np.asarray(feats[feat_str]["hpcp"])
+    M = np.asarray(feats[feat_str]["mfcc"])
+    T = np.asarray(feats[feat_str]["tonnetz"])
+    analysis = feats["analysis"]
 
     # Duration
     dur = jam.metadata.duration
 
-    f.close()
-
-    return C, M, beats, dur
+    return C, M, T, beats, dur, analysis
 
 
 def create_estimation(times, annot_beats, annot_bounds, **params):
@@ -444,7 +381,7 @@ def get_all_est_boundaries(est_file, annot_beats, algo_ids=None):
     all_boundaries = []
 
     # Get GT boundaries
-    jam_file = os.path.dirname(est_file) + "/../annotations/" + \
+    jam_file = os.path.dirname(est_file) + "/../references/" + \
         os.path.basename(est_file).replace("json", "jams")
     ds_prefix = os.path.basename(est_file).split("_")[0]
     ann_inter, ann_labels = jams2.converters.load_jams_range(jam_file,
@@ -490,7 +427,8 @@ def get_all_est_labels(est_file, annot_beats, algo_ids=None):
     all_labels = []
 
     # Get GT boundaries and labels
-    jam_file = os.path.dirname(est_file) + "/../annotations/" + \
+    jam_file = os.path.dirname(est_file) + "/../" + \
+        msaf.Dataset.references_dir +"/" + \
         os.path.basename(est_file).replace("json", "jams")
     ds_prefix = os.path.basename(est_file).split("_")[0]
     ann_inter, ann_labels = jams2.converters.load_jams_range(
