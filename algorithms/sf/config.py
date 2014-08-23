adea7b8346eb0d5f8c88c30c1b3654ee0790ad15
Uri Nieto <oriol.nieto@gmail.com>
2014-08-23 10:23:16 -0400

    SF method working
    Former-commit-id: 632547001e112bdd5e0776161b565808deef69bf
    Former-commit-id: 6a945bdb50be6bd48714bd5825c2b73e2e873cab

---
+++ algorithms/sf/config.py
@@ -0,0 +1,14 @@
+"""Config for the Structural Features algorithm."""
+
+# Serra params
+config = {
+    "M_gaussian"    : 16,
+    "m_embedded"    : 3,
+    "k_nearest"     : 0.06,
+    "Mp_adaptive"   : 24,
+    "offset_thres"  : 0.04
+}
+
+algo_id = "sf"
+is_boundary_type = True
+is_label_type = False
