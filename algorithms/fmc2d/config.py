8517bbdb5a8a1a770b445c5d8d08791e23cc2567
Uri Nieto <oriol.nieto@gmail.com>
2014-08-23 09:25:07 -0400

    Removed old C-NMF algorithms
    Former-commit-id: 61b3257e273f86cb4c23d94746c18fedfeac21ba
    Former-commit-id: 40d83f69c8c8a92867ceb2dc584269a03c40b71f

--- algorithms/fmc2d/config.py
+++ algorithms/fmc2d/config.py
@@ -2,14 +2,11 @@
 
 # 2D-FMC Params
 config = {
-    "niter"             :   200,
-    "alphaZ"            :   -0.01,
-    "normalize_frames"  :   True,
-    "viterbi_segmenter" :   True,
-    "min_segment_length":   32
+    "xmeans"    :   False,
+    "k"         :   6
 }
 
 # Other params
-algo_id = "2dfmc"
+algo_id = "fmc2d"
 is_boundary_type = False
 is_label_type = True
