b5d5f0ba6daa2ba0045298402b6f3cf999da474c
Uri Nieto <oriol.nieto@gmail.com>
2014-12-06 10:44:58 -0500

    Trying to fix 2d-fmc

--- run.py
+++ run.py
@@ -191,7 +191,7 @@ def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
         # Call in parallel
         return Parallel(n_jobs=n_jobs)(delayed(process_track)(
             file_struct, boundaries_id, labels_id, config)
-            for file_struct in file_structs[1441:])
+            for file_struct in file_structs[1444:])
 
 
 def main():
