78543ff4d91693253191f086560efa62d23a3343
Oriol Nieto <oriol.nieto@gmail.com>
2015-02-20 15:37:50 -0500

    Added a setup to install msaf
    Former-commit-id: d212296a305ef5494dda6f0c0b6960049dc22727
    Former-commit-id: 683461bd8cc2a33fb789406c5c9058af5d2dc050

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+setup(
+        name='msaf',
+        version='0.0.2',
+        description='Python module to discover the structure of music files',
+        author='Oriol Nieto',
+        author_email='oriol@nyu.edu',
+        url='https://github.com/urinieto/msaf',
+        download_url='https://github.com/urinieto/msaf/releases',
+        packages=['msaf'],
+        package_data={'': ['ds_example/*']},
+        long_description="""A python module to segment audio into all its """
+        """different large-scale sections and label them based on their """
+        """acoustic similarity""",
+        classifiers=[
+"License :: OSI Approved :: GPL 3",
+"Programming Language :: Python",
+"Development Status :: 3 - Alpha",
+"Intended Audience :: Developers",
+"Topic :: Multimedia :: Sound/Audio :: Analysis",
+"Programming Language :: Python :: 2",
+"Programming Language :: Python :: 2.7",
+],
+        keywords='audio music sound',
+        license='GPL',
+        install_requires=[
+'audioread',
+'numpy >= 1.8.0',
+'scipy >= 0.13.0',
+'scikit-learn >= 0.14.0',
+'matplotlib',
+'joblib',
+'decorator',
+'cvxopt',
+'pymf',
+'joblib',
+'librosa',
+'mir_eval',
+'pandas',
+],
+        extras_require={
+'resample': 'scikits.samplerate>=0.3'
+}
+        )
