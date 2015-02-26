cb3f5a1606f7409d6c651e8b2668e797bbced1ef
Oriol Nieto <oriol.nieto@gmail.com>
2015-02-26 12:33:13 -0500

    Checking duration in tests
    Former-commit-id: 123ad9898aebc3364bd864b14a249b8520c2bf14
    Former-commit-id: 7aaa4cfbdffd1f2b3e7341d0ba74ab84f122cc1e

--- tests/test_featextract.py
+++ tests/test_featextract.py
@@ -5,7 +5,9 @@
 # nosetests
 
 import glob
-from nose.tools import nottest, eq_, raises
+import json
+from nose.tools import nottest, eq_, raises, assert_equals
+import numpy.testing as npt
 import os
 
 # Msaf imports
@@ -13,8 +15,16 @@ import msaf.featextract
 
 
 def test_save_features():
+    # Read audio and compute features
     tmp_file = "temp.json"
     audio_file = os.path.join("data", "chirp.mp3")
     features = msaf.featextract.compute_features_for_audio_file(audio_file)
     msaf.featextract.save_features(tmp_file, features)
+
+    # Check that the json file is actually readable
+    with open(tmp_file, "r") as f:
+        features = json.load(f)
+    npt.assert_almost_equal(features["analysis"]["dur"], 10, decimal=1)
+
+    # Clean up
     os.remove(tmp_file)
