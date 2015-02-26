65062aead3c592abaf6a54f17a95336b22a44f4f
Oriol Nieto <oriol.nieto@gmail.com>
2015-02-26 15:04:20 -0500

    Removed old scripts. Split eval to script and module. Added README to pymf module
    Former-commit-id: 5c30c6b6d2aac7f8b79b19476219b82843af88ef
    Former-commit-id: 1d0fbc095f48c766316aef9be8c2fe5977294567

--- msaf/eval.py
+++ msaf/eval.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python
 """
 Evaluates the estimated results of the Segmentation dataset against the
 ground truth (human annotated data).
 """
 
-__author__      = "Oriol Nieto"
-__copyright__   = "Copyright 2014, Music and Audio Research Lab (MARL)"
-__license__     = "GPL"
-__version__     = "1.0"
-__email__       = "oriol@nyu.edu"
-
-import argparse
 from joblib import Parallel, delayed
 import logging
 import mir_eval
 import os
 import pandas as pd
 import sys
-import time
 
 # Local stuff
 import msaf
@@ -286,77 +277,3 @@ def process(in_path, boundaries_id, labels_id=None, ds_name="*",
         results.to_csv(out_file)
 
     return results
-
-
-def main():
-    """Main function to parse the arguments and call the main process."""
-    parser = argparse.ArgumentParser(description=
-        "Evaluates the estimated results of the Segmentation dataset",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("in_path",
-                        action="store",
-                        help="Input dataset")
-    parser.add_argument("-bid",
-                        action="store",
-                        help="Boundary algorithm identifier",
-                        dest="boundaries_id",
-                        default="gt",
-                        choices=["gt"] +
-                        io.get_all_boundary_algorithms(algorithms))
-    parser.add_argument("-lid",
-                        action="store",
-                        help="Label algorithm identifier",
-                        dest="labels_id",
-                        default=None,
-                        choices= io.get_all_label_algorithms(algorithms))
-    parser.add_argument("-d",
-                        action="store",
-                        dest="ds_name",
-                        default="*",
-                        help="The prefix of the dataset to use "
-                        "(e.g. Isophonics, SALAMI")
-    parser.add_argument("-b",
-                        action="store_true",
-                        dest="annot_beats",
-                        help="Use annotated beats",
-                        default=False)
-    parser.add_argument("-fs",
-                        action="store_true",
-                        dest="framesync",
-                        default=False,
-                        help="Whether to use framesync features or not")
-    parser.add_argument("-f",
-                        action="store",
-                        dest="feature",
-                        default="hpcp",
-                        type=str,
-                        help="Type of features",
-                        choices=["hpcp", "tonnetz", "mfcc"])
-    parser.add_argument("-s",
-                        action="store_true",
-                        dest="save",
-                        help="Whether to save the results or not",
-                        default=False)
-    parser.add_argument("-j",
-                        action="store",
-                        dest="n_jobs",
-                        default=4,
-                        type=int,
-                        help="The number of processes to run in parallel")
-    args = parser.parse_args()
-    start_time = time.time()
-
-    # Setup the logger
-    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
-                        level=logging.INFO)
-
-    # Run the algorithm
-    process(args.in_path, args.boundaries_id, args.labels_id, args.ds_name,
-            args.annot_beats, save=args.save, feature=args.feature,
-            n_jobs=args.n_jobs, framesync=args.framesync)
-
-    # Done!
-    logging.info("Done! Took %.2f seconds." % (time.time() - start_time))
-
-if __name__ == '__main__':
-    main()
