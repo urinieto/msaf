73dd24468354e9f3e8a30a83bff36bc346b636ed
Oriol Nieto <oriol.nieto@gmail.com>
2015-02-27 18:39:08 -0500

    Added more tests for featextract
    Former-commit-id: 75fb361b51f0d77cc6c5b2824ab5b7b5912066fa
    Former-commit-id: 8192ff6289eff1837b12afb8050a831a1eaab4e6

--- msaf/utils.py
+++ msaf/utils.py
@@ -44,43 +44,6 @@ def ensure_dir(directory):
         os.makedirs(directory)
 
 
-def resample_mx(X, incolpos, outcolpos):
-    """
-    Y = resample_mx(X, incolpos, outcolpos)
-    X is taken as a set of columns, each starting at 'time'
-    colpos, and continuing until the start of the next column.
-    Y is a similar matrix, with time boundaries defined by
-    outcolpos.  Each column of Y is a duration-weighted average of
-    the overlapping columns of X.
-    2010-04-14 Dan Ellis dpwe@ee.columbia.edu  based on samplemx/beatavg
-    -> python: TBM, 2011-11-05, TESTED
-    """
-    noutcols = len(outcolpos)
-    Y = np.zeros((X.shape[0], noutcols))
-    # assign 'end times' to final columns
-    if outcolpos.max() > incolpos.max():
-        incolpos = np.concatenate([incolpos, [outcolpos.max()]])
-        X = np.concatenate([X, X[:, -1].reshape(X.shape[0], 1)], axis=1)
-    outcolpos = np.concatenate([outcolpos, [outcolpos[-1]]])
-    # durations (default weights) of input columns)
-    incoldurs = np.concatenate([np.diff(incolpos), [1]])
-
-    for c in range(noutcols):
-        firstincol = np.where(incolpos <= outcolpos[c])[0][-1]
-        firstincolnext = np.where(incolpos < outcolpos[c+1])[0][-1]
-        lastincol = max(firstincol, firstincolnext)
-        # default weights
-        wts = copy.deepcopy(incoldurs[firstincol:lastincol+1])
-        # now fix up by partial overlap at ends
-        if len(wts) > 1:
-            wts[0] = wts[0] - (outcolpos[c] - incolpos[firstincol])
-            wts[-1] = wts[-1] - (incolpos[lastincol+1] - outcolpos[c+1])
-        wts = wts * 1. / sum(wts)
-        Y[:, c] = np.dot(X[:, firstincol:lastincol+1], wts)
-    # done
-    return Y
-
-
 def chroma_to_tonnetz(C):
     """Transforms chromagram to Tonnetz (Harte, Sandler, 2006)."""
     N = C.shape[0]
