3a1af7fd3c5b82079e7887500e11c98d565fe008
Oriol Nieto <oriol.nieto@gmail.com>
2015-02-27 20:08:43 -0500

    Trying to compile in travis
    Former-commit-id: b53ee5ef829a983e397f14a41a58445b8e0831e8
    Former-commit-id: e7ccb659a4a4035772c1a08191c876e6c6ab1de5

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ print sys.platform
 # Compile the CC algorithm
 extra_compile_flags = ""
 extra_linker_flags = ""
-if "linux" in sys.platform:
+if sysconfig.get_config_var('LDSHARED').split(" ")[0] == "gcc":
+    extra_compile_flags = "-DUSE_PTHREADS"
+    extra_linker_flags = "-llapack -lblas -lm"
+elif "linux" in sys.platform:
     extra_compile_flags = "-std=c++11 -DUSE_PTHREADS"
     extra_linker_flags = "-llapack -lblas -lm"
 elif "darwin" in sys.platform:
