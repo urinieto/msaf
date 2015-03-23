b8c12c74c7be21097d8c92f62c4477f2fdfab41b
Oriol Nieto <oriol.nieto@gmail.com>
2015-03-23 17:02:35 -0400

    The C-NMF3 method is now called cnmf

---
+++ msaf/algorithms/cnmf/config.py
@@ -0,0 +1,13 @@
+"""Configuration for the C-NMF algorithm."""
+
+config = {
+    "h" : 20,
+    "R" : 16,
+    "rank" : 3,
+    "R_labels" : 16,
+    "rank_labels" : 4
+}
+
+algo_id = "cnmf"
+is_boundary_type = True
+is_label_type = True
