Fixed a few bugs regarding the computation of framesync features
Former-commit-id: a829d84a0b0b138dfeb88c2a4b4bd533e2d49bf0
Former-commit-id: dc53ef10a2c4cb1bdc2d94b4fe041815dc6df13f

msaf/algorithms/foote/segmenter.py
@@ -69,13 +69,12 @@ def compute_nc(X, G):
 
 def pick_peaks(nc, L=16):
     """Obtain peaks from a novelty curve using an adaptive threshold."""
-    offset = nc.mean() / 2.
+    offset = nc.mean() / 20.
+
+    nc = filters.gaussian_filter1d(nc, sigma=4)  # Smooth out nc
 
     th = filters.median_filter(nc, size=L) + offset
     #th = filters.gaussian_filter(nc, sigma=L/2., mode="nearest") + offset
-
-    nc = filters.gaussian_filter1d(nc, sigma=2)  # Hack for Musichackathon
-    th = np.zeros(len(nc))  # Hack continues
 
     peaks = []
     for i in xrange(1, nc.shape[0] - 1):
@@ -105,6 +104,10 @@ class Segmenter(SegmenterInterface):
         """
         # Preprocess to obtain features
         F = self._preprocess()
+
+        # Make sure that the M_gaussian is even
+        if self.config["M_gaussian"] % 2 == 1:
+            self.config["M_gaussian"] += 1
 
         # Median filter
         F = median_filter(F, M=self.config["m_median"])

msaf/eval.py
@@ -173,8 +173,6 @@ def compute_gt_results(est_file, ref_file, boundaries_id, labels_id, config,
             est_labels.append(np.ones(len(est_times[-1]) - 1) * -1)
 
         # Align the times
-        #if "What_Goes" in est_file:
-            #import pdb; pdb.set_trace()  # XXX BREAKPOINT
         utils.align_end_hierarchies(est_times, ref_times)
 
         # Build trees
@@ -337,7 +335,7 @@ def process(in_path, boundaries_id, labels_id=None, ds_name="*",
 
     # Save all results
     if save:
-        logging.info("Writing average results in %s" % out_file)
+        logging.info("Writing results in %s" % out_file)
         results.to_csv(out_file)
 
     return results