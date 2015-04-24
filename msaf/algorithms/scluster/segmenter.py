3130debcbd600238a8c0a3f724211ea8a61f7441
Uri Nieto <oriol.nieto@gmail.com>
2015-04-24 17:15:57 -0400

    Fixed scluster to work with new cqt features
    Former-commit-id: a736c7e4aac40e00e3409e67830e9e77ae6a1cf2
    Former-commit-id: 1b05136fadc7555232eac08c31f58fdb93a889a4

--- msaf/algorithms/scluster/segmenter.py
+++ msaf/algorithms/scluster/segmenter.py
@@ -42,7 +42,7 @@ class Segmenter(SegmenterInterface):
         F = self._preprocess()
 
         # Read frame_times
-        self.hpcp, self.mfcc, self.tonnetz, beats, dur, self.anal = \
+        self.hpcp, self.mfcc, self.tonnetz, self.cqt, beats, dur, self.anal = \
             msaf.io.get_features(self.audio_file, annot_beats=self.annot_beats,
                                  framesync=self.framesync,
                                  pre_features=self.features)
