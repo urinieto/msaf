22d58c96f6417daeb8a8dd0c8dfc706c7a451637
Oriol Nieto <onieto@pandora.com>
2016-02-26 14:28:48 -0800

    Added algorithm example to facilitate inclusion of new algorithms
    Former-commit-id: 6698579563929c25d3d4797dd1ba6523cdb7f51f
    Former-commit-id: c48c729c17eba8edf62050177522861b00059c66

---
+++ msaf/algorithms/example/config.py
@@ -0,0 +1,11 @@
+"""Example of a configuration file for a new algorithm"""
+
+# Algorithm Params
+config = {
+    "my_param1": 1.0,
+    "my_param2": 2.0
+}
+
+algo_id = "example"  # Identifier of the algorithm
+is_boundary_type = True  # Whether the algorithm extracts boundaries
+is_label_type = False  # Whether the algorithm labels segments
