3ce0c8c0504e289c60bcfdc72ee22f049b5ae244
Oriol Nieto <onieto@pandora.com>
2016-05-22 11:18:11 -0700

    adding first and last boundary, and not padding the beat-synchronous process
    Former-commit-id: d4b132403465438db250888c07763bd625a4f647
    Former-commit-id: a7a94e5c77a3369c87524baaece5c2c36bd1aeb5

--- msaf/featextract.py
+++ msaf/featextract.py
@@ -46,11 +46,9 @@ def compute_beats(y_percussive, sr=22050):
     tempo, beats_idx = librosa.beat.beat_track(y=y_percussive, sr=sr,
                                                hop_length=msaf.Anal.hop_size)
 
-    # If librosa couldn't track any beat, simply add the first and last
+    # Add first and last beat
     beats_idx = np.concatenate(([0], beats_idx,
-                                [len(y_percussive) / msaf.Anal.hop_size - 1]))
-    # if tempo == 0:
-        # beats_idx = np.array([0, len(y_percussive) - 1])
+                                [len(y_percussive) / msaf.Anal.hop_size]))
 
     # To times
     times = librosa.frames_to_time(beats_idx, sr=sr,
@@ -168,7 +166,7 @@ def save_features(out_file, features):
 def compute_beat_sync_features(features, beats_idx):
     """Given a dictionary of features, and the estimated index frames,
     calculate beat-synchronous features."""
-    pad = True
+    pad = False
     bs_mfcc = librosa.feature.sync(features["mfcc"].T, beats_idx, pad=pad).T
     bs_hpcp = librosa.feature.sync(features["hpcp"].T, beats_idx, pad=pad).T
     bs_tonnetz = librosa.feature.sync(features["tonnetz"].T, beats_idx,
