0b83b3797a30308318ccf26b838ced6df1f42b7b
Oriol Nieto <oriol.nieto@gmail.com>
2016-06-05 10:03:28 -0700

    Added rest of the features. Cleaned up __init__ a bit
    Former-commit-id: 4b3189da5e3903d1e08c432655d301a8e746ba19
    Former-commit-id: 01fa92ff15b5e1bb2f2943003e116d22ba094493

--- msaf/features.py
+++ msaf/features.py
@@ -12,7 +12,6 @@ import numpy as np
 # Local stuff
 import msaf
 from msaf import utils
-from msaf import input_output as io
 from msaf.input_output import FileStruct
 from msaf.base import Features
 
@@ -143,111 +142,178 @@ class MFCC(Features):
         return mfcc
 
 
-#def compute_features(audio, y_harmonic):
-    #"""Computes the HPCP and MFCC features.
+class PCP(Features):
+    """This class contains the implementation of the Pitch Class Profiles.
 
-    #Parameters
-    #----------
-    #audio: np.array(N)
-        #Audio samples of the given input.
-    #y_harmonic: np.array(N)
-        #Harmonic part of the audio signal, in samples.
+    The PCPs contain harmonic content of a given audio signal.
+    """
+    def __init__(self, file_struct, feat_type, sr=msaf.Anal.sample_rate,
+                 hop_length=msaf.Anal.hop_size, n_bins=msaf.Anal.cqt_bins,
+                 norm=msaf.Anal.cqt_norm, f_min=msaf.Anal.f_min,
+                 n_octaves=msaf.Anal.n_octaves):
+        """Constructor of the class.
 
-    #Returns
-    #-------
-    #mfcc: np.array(N, msaf.Anal.mfcc_coeff)
-        #Mel-frequency Cepstral Coefficients.
-    #pcp: np.array(N, 12)
-        #Pitch Class Profiles.
-    #tonnetz: np.array(N, 6)
-        #Tonal Centroid features.
-    #cqt: np.array(N, msaf.Anal.cqt_bins)
-        #Constant-Q log-scale features.
-    #tempogram: np.array(N, 192)
-        #Tempogram features.
-    #"""
-    #logging.info("Computing Spectrogram...")
-    ##S = librosa.feature.melspectrogram(audio,
-                                       ##sr=msaf.Anal.sample_rate,
-                                       ##n_fft=msaf.Anal.frame_size,
-                                       ##hop_length=msaf.Anal.hop_size,
-                                       ##n_mels=msaf.Anal.n_mels)
+        Parameters
+        ----------
+        file_struct: `msaf.input_output.FileStruct`
+            Object containing the file paths from where to extract/read
+            the features.
+        feat_type: `FeatureTypes`
+            Enum containing the type of features.
+        sr: int > 0
+            Sampling rate for the analysis.
+        hop_length: int > 0
+            Hop size in frames for the analysis.
+        n_bins: int > 0
+            Number of bins for the CQT computation.
+        norm: int > 0
+            Normalization parameter.
+        f_min: float > 0
+            Minimum frequency.
+        n_octaves: int > 0
+            Number of octaves.
+        """
+        # Init the parent
+        super().__init__(file_struct=file_struct, sr=sr, hop_length=hop_length,
+                         feat_type=feat_type)
+        # Init the PCP parameters
+        self.n_bins = n_bins
+        self.norm = norm
+        self.f_min = f_min
+        self.n_octaves = n_octaves
 
-    #logging.info("Computing Constant-Q...")
-    ##cqt = librosa.logamplitude(librosa.cqt(audio, sr=msaf.Anal.sample_rate,
-                                           ##hop_length=msaf.Anal.hop_size,
-                                           ##n_bins=msaf.Anal.cqt_bins) ** 2,
-                               ##ref_power=np.max).T
+    def get_id(self):
+        """Identifier of these features."""
+        return "pcp"
 
-    ##linear_cqt = np.abs(librosa.cqt(y_harmonic,
-                                    ##sr=msaf.Anal.sample_rate,
-                                    ##hop_length=msaf.Anal.hop_size,
-                                    ##n_bins=msaf.Anal.cqt_bins,
-                                    ##norm=np.inf,
-                                    ##filter_scale=1,
-                                    ##real=False))
-    ##cqt = librosa.logamplitude(linear_cqt, ref_power=np.max).T
+    def compute_features(self):
+        """Actual implementation of the features.
 
-    #logging.info("Computing MFCCs...")
-    ##log_S = librosa.logamplitude(S, ref_power=np.max)
-    ##mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=msaf.Anal.mfcc_coeff).T
-
-    #logging.info("Computing PCPs...")
-    #pcp_cqt = np.abs(librosa.hybrid_cqt(y_harmonic,
-                                        #sr=msaf.Anal.sample_rate,
-                                        #hop_length=msaf.Anal.hop_size,
-                                        #n_bins=msaf.Anal.cqt_bins,
-                                        #norm=1,
-                                        #fmin=f_min)) ** 2
-    #pcp = librosa.feature.chroma_cqt(C=pcp_cqt,
-                                     #sr=msaf.Anal.sample_rate,
-                                     #hop_length=msaf.Anal.hop_size,
-                                     #n_octaves=msaf.Anal.n_octaves,
-                                     #fmin=f_min).T
-    ##pcp = librosa.feature.chroma_cqt(C=linear_cqt,
-                                     ##sr=msaf.Anal.sample_rate,
-                                     ##hop_length=msaf.Anal.hop_size,
-                                     ##n_octaves=msaf.Anal.n_octaves,
-                                     ##fmin=msaf.Anal.f_min).T
-    ##pcp = librosa.feature.chroma_stft(y=y_harmonic,
-                                      ##sr=msaf.Anal.sample_rate,
-                                      ##n_fft=msaf.Anal.frame_size,
-                                      ##hop_length=msaf.Anal.hop_size).T
-
-    #logging.info("Computing Tonnetz...")
-    ##tonnetz = utils.chroma_to_tonnetz(pcp)
-
-    ## TODO:
-    #tonnetz = pcp
-    #mfcc = pcp
-    #cqt = pcp
-
-    #logging.info("Computing Tempogram...")
-    #tempogram = librosa.feature.tempogram(audio,
-                                          #sr=msaf.Anal.sample_rate,
-                                          #hop_length=msaf.Anal.hop_size,
-                                          #win_length=192).T
-    #return mfcc, pcp, tonnetz, cqt, tempogram
+        Returns
+        -------
+        pcp: np.array(N, F)
+            The features, each row representing a feature vector for a give
+            time frame/beat.
+        """
+        audio_harmonic, _ = self.compute_HPSS()
+        pcp_cqt = np.abs(librosa.hybrid_cqt(audio_harmonic,
+                                            sr=self.sr,
+                                            hop_length=self.hop_length,
+                                            n_bins=self.n_bins,
+                                            norm=self.norm,
+                                            fmin=self.f_min)) ** 2
+        pcp = librosa.feature.chroma_cqt(C=pcp_cqt,
+                                         sr=self.sr,
+                                         hop_length=self.hop_length,
+                                         n_octaves=self.n_octaves,
+                                         fmin=self.f_min).T
+        return pcp
 
 
+class Tonnetz(Features):
+    """This class contains the implementation of the Tonal Centroids.
+
+    The Tonal Centroids (or Tonnetz) contain harmonic content of a given audio
+    signal.
+    """
+    def __init__(self, file_struct, feat_type, sr=msaf.Anal.sample_rate,
+                 hop_length=msaf.Anal.hop_size, n_bins=msaf.Anal.cqt_bins,
+                 norm=msaf.Anal.cqt_norm, f_min=msaf.Anal.f_min,
+                 n_octaves=msaf.Anal.n_octaves):
+        """Constructor of the class.
+
+        Parameters
+        ----------
+        file_struct: `msaf.input_output.FileStruct`
+            Object containing the file paths from where to extract/read
+            the features.
+        feat_type: `FeatureTypes`
+            Enum containing the type of features.
+        sr: int > 0
+            Sampling rate for the analysis.
+        hop_length: int > 0
+            Hop size in frames for the analysis.
+        n_bins: int > 0
+            Number of bins for the CQT computation.
+        norm: int > 0
+            Normalization parameter.
+        f_min: float > 0
+            Minimum frequency.
+        n_octaves: int > 0
+            Number of octaves.
+        """
+        # Init the parent
+        super().__init__(file_struct=file_struct, sr=sr, hop_length=hop_length,
+                         feat_type=feat_type)
+        # Init the local parameters
+        self.n_bins = n_bins
+        self.norm = norm
+        self.f_min = f_min
+        self.n_octaves = n_octaves
+
+    def get_id(self):
+        """Identifier of these features."""
+        return "tonnetz"
+
+    def compute_features(self):
+        """Actual implementation of the features.
+
+        Returns
+        -------
+        tonnetz: np.array(N, F)
+            The features, each row representing a feature vector for a give
+            time frame/beat.
+        """
+        pcp = PCP(self.file_struct, self.feat_type, self.sr, self.hop_length,
+                  self.n_bins, self.norm, self.f_min, self.n_octaves).features
+        tonnetz = librosa.feature.tonnetz(chroma=pcp)
+        return tonnetz
 
 
-#def compute_beat_sync_features(features, beats_idx):
-    #"""Given a dictionary of features, and the estimated index frames,
-    #calculate beat-synchronous features."""
-    #pad = True  # Always pad til the end of the actual audio file
-    #bs_mfcc = librosa.feature.sync(features["mfcc"].T, beats_idx, pad=pad).T
-    #bs_pcp = librosa.feature.sync(features["pcp"].T, beats_idx, pad=pad).T
-    #bs_tonnetz = librosa.feature.sync(features["tonnetz"].T, beats_idx,
-                                      #pad=pad).T
-    #bs_cqt = librosa.feature.sync(features["cqt"].T, beats_idx, pad=pad).T
-    #bs_tempogram = librosa.feature.sync(features["tempogram"].T, beats_idx,
-                                        #pad=pad).T
+class Tempogram(Features):
+    """This class contains the implementation of the Tempogram feature.
 
-    ## Make sure we have the right size (remove last frame if needed)
-    #bs_mfcc = bs_mfcc[:len(beats_idx), :]
-    #bs_pcp = bs_pcp[:len(beats_idx), :]
-    #bs_tonnetz = bs_tonnetz[:len(beats_idx), :]
-    #bs_cqt = bs_cqt[:len(beats_idx), :]
-    #bs_tempogram = bs_tempogram[:len(beats_idx), :]
-    #return bs_mfcc, bs_pcp, bs_tonnetz, bs_cqt, bs_tempogram
+    The Tempogram contains rhythmic content of a given audio signal.
+    """
+    def __init__(self, file_struct, feat_type, sr=msaf.Anal.sample_rate,
+                 hop_length=msaf.Anal.hop_size,
+                 win_length=sr.msaf.Anal.win_length):
+        """Constructor of the class.
+
+        Parameters
+        ----------
+        file_struct: `msaf.input_output.FileStruct`
+            Object containing the file paths from where to extract/read
+            the features.
+        feat_type: `FeatureTypes`
+            Enum containing the type of features.
+        sr: int > 0
+            Sampling rate for the analysis.
+        hop_length: int > 0
+            Hop size in frames for the analysis.
+        win_length: int > 0
+            The size of the window for the tempogram.
+        """
+        # Init the parent
+        super().__init__(file_struct=file_struct, sr=sr, hop_length=hop_length,
+                         feat_type=feat_type)
+        # Init the local parameters
+        self.win_length = win_length
+
+    def get_id(self):
+        """Identifier of these features."""
+        return "tempogram"
+
+    def compute_features(self):
+        """Actual implementation of the features.
+
+        Returns
+        -------
+        tempogram: np.array(N, F)
+            The features, each row representing a feature vector for a give
+            time frame/beat.
+        """
+        tempogram = librosa.feature.tempogram(self._audio,
+                                              sr=self.sr,
+                                              hop_length=self.hop_length,
+                                              win_length=self.win_length).T
