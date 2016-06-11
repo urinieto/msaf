04641d9af748167f609f7633aea9882af3aea5d0
Oriol Nieto <oriol.nieto@gmail.com>
2016-06-11 13:01:06 -0700

    Adding more variables and more tests #23
    Former-commit-id: d2a89e5a8d4e82e12b973c2dce891b1e1054047a
    Former-commit-id: 7b111b0a63a2ee2521526edabaae87a540cf9f80

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,8 +10,29 @@ import os
 # Msaf imports
 import msaf
 
+from msaf.configparser import (AddConfigVar, BoolParam, ConfigParam, EnumStr,
+                               FloatParam, IntParam, StrParam,
+                               MsafConfigParser, MSAF_FLAGS_DICT)
+
+
+def test_add_var():
+    """Adds a config variable and checks that it's correctly stored."""
+    val = 10
+    AddConfigVar('test.new_var', "Test Variable only for unit testing",
+                 IntParam(val))
+
+    assert msaf.config.test.new_var == val
+
+
+@raises(AttributeError)
+def test_no_var():
+    """Raises an AttributeError if the variable doesn't exist in the config."""
+    _ = msaf.config.wrong_variable_name
+
 
 def test_config():
     """All the features should be in the features register."""
     print(msaf.config)
     print(msaf.config.sample_rate)
+    print(msaf.config.cqt.bins)
+
