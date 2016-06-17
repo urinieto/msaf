3eace47776764961d5a3992f1d136cb201fcef68
Oriol Nieto <onieto@pandora.com>
2016-06-17 08:11:39 -0700

    Adjusted eval with the new config defaults

--- msaf/eval.py
+++ msaf/eval.py
@@ -301,8 +301,8 @@ def process_track(file_struct, boundaries_id, labels_id, config,
 def get_results_file_name(boundaries_id, labels_id, config, annotator_id):
     """Based on the config and the dataset, get the file name to store the
     results."""
-    utils.ensure_dir(msaf.results_dir)
-    file_name = os.path.join(msaf.results_dir, "results")
+    utils.ensure_dir(msaf.config.results_dir)
+    file_name = os.path.join(msaf.config.results_dir, "results")
     file_name += "_boundsE%s_labelsE%s" % (boundaries_id, labels_id)
     file_name += "_annotatorE%d" % (annotator_id)
     sorted_keys = sorted(config.keys(), key=str.lower)
@@ -310,10 +310,10 @@ def get_results_file_name(boundaries_id, labels_id, config, annotator_id):
         file_name += "_%sE%s" % (key, str(config[key]).replace("/", "_"))
 
     # Check for max file length
-    if len(file_name) > 255 - len(msaf.results_ext):
-        file_name = file_name[:255 - len(msaf.results_ext)]
+    if len(file_name) > 255 - len(msaf.config.results_ext):
+        file_name = file_name[:255 - len(msaf.config.results_ext)]
 
-    return file_name + msaf.results_ext
+    return file_name + msaf.config.results_ext
 
 
 def process(in_path, boundaries_id=msaf.config.default_bound_id,
