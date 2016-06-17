cce4d147f4e0472b09f4f383311252aa6d60f46b
Oriol Nieto <onieto@pandora.com>
2016-06-17 15:31:05 -0700

    Adding script to run MIREX

---
+++ examples/run_mirex.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+"""
+Runs one boundary algorithm and a label algorithm on a specified audio file
+and outputs the results using the MIREX format.
+"""
+import argparse
+import logging
+import time
+
+# MSAF import
+import msaf
+
+
+def write_mirex(times, labels, out_file):
+    """Writes results to file."""
+    inters = msaf.utils.times_to_intervals(times)
+    assert len(inters) == len(labels)
+    out_str = ""
+    for inter, label in zip(inters, labels):
+        out_str += "%.3f\t%.3f\t%s\n" % (inter[0], inter[1], label)
+    with open(out_file, "w") as f:
+        f.write(out_str[:-1])
+
+
+def main():
+    """Main function to parse the arguments and call the main process."""
+    parser = argparse.ArgumentParser(
+        description="Runs the speficied algorithm(s) on the input file and "
+        "the results using the MIREX format.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-bid",
+                        action="store",
+                        help="Boundary algorithm identifier",
+                        dest="boundaries_id",
+                        default=msaf.config.default_bound_id,
+                        choices=["gt"] +
+                        msaf.io.get_all_boundary_algorithms())
+    parser.add_argument("-lid",
+                        action="store",
+                        help="Label algorithm identifier",
+                        dest="labels_id",
+                        default=msaf.config.default_label_id,
+                        choices=msaf.io.get_all_label_algorithms())
+    parser.add_argument("-i",
+                        action="store",
+                        dest="in_file",
+                        help="Input audio file")
+    parser.add_argument("-o",
+                        action="store",
+                        dest="out_file",
+                        help="Output file with the results",
+                        default="out.txt")
+
+    args = parser.parse_args()
+    start_time = time.time()
+
+    # Setup the logger
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
+                        level=logging.INFO)
+
+    # Run MSAF
+    params = {
+        "annot_beats": False,
+        "feature": "cqt",
+        "framesync": False,
+        "boundaries_id": args.boundaries_id,
+        "labels_id": args.labels_id,
+        "n_jobs": 1,
+        "hier": False,
+        "sonify_bounds": False,
+        "plot": False
+    }
+    res = msaf.run.process(args.in_file, **params)
+    write_mirex(res[0], res[1], args.out_file)
+
+    # Done!
+    logging.info("Done! Took %.2f seconds." % (time.time() - start_time))
+
+
+if __name__ == '__main__':
+    main()
