093f9c2781d36cdda66992f1025f76632777bf93
Oriol Nieto <onieto@pandora.com>
2016-08-04 18:37:18 -0500

    Fixed some ol variable problems, fixed PEP8 style stuff

--- examples/run_sweep.py
+++ examples/run_sweep.py
@@ -8,12 +8,10 @@ import numpy as np
 
 import msaf
 from msaf import input_output as io
-import msaf.algorithms as algorithms
 
 
-def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
-            framesync=False, boundaries_id="gt", labels_id=None, n_jobs=4,
-            config=None):
+def process(in_path, annot_beats=False, feature="mfcc", framesync=False,
+            boundaries_id="gt", labels_id=None, n_jobs=4, config=None):
     """Sweeps parameters across the specified algorithm."""
 
     results_file = "results_sweep_boundsE%s_labelsE%s.csv" % (boundaries_id,
@@ -42,13 +40,15 @@ def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
                             config["features"] = None
 
                             # Run process
-                            msaf.run.process(in_path, n_jobs=n_jobs, 
-                                             boundaries_id=boundaries_id, 
-                                             labels_id=labels_id, config=config)
+                            msaf.run.process(
+                                in_path, n_jobs=n_jobs,
+                                boundaries_id=boundaries_id,
+                                labels_id=labels_id, config=config)
 
                             # Compute evaluations
-                            results = msaf.eval.process(in_path, boundaries_id, labels_id, 
-                                                        save=True, n_jobs=n_jobs, config=config)
+                            results = msaf.eval.process(
+                                in_path, boundaries_id, labels_id,
+                                save=True, n_jobs=n_jobs, config=config)
 
                             # Save avg results
                             new_columns = {"config_h": h, "config_R": R,
@@ -56,9 +56,9 @@ def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
                                            "config_R_labels": R_labels,
                                            "config_rank_labels": rank_labels}
                             results = results.append([new_columns],
-                                                    ignore_index=True)
+                                                     ignore_index=True)
                             all_results = all_results.append(results.mean(),
-                                                            ignore_index=True)
+                                                             ignore_index=True)
                             all_results.to_csv(results_file)
 
     elif labels_id is None and boundaries_id == "sf":
@@ -84,23 +84,24 @@ def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
                             config["features"] = None
 
                             # Run process
-                            msaf.run.process(in_path, ds_name=ds_name, n_jobs=n_jobs,
-                                        boundaries_id=boundaries_id,
-                                        labels_id=labels_id, config=config)
+                            msaf.run.process(
+                                in_path, n_jobs=n_jobs,
+                                boundaries_id=boundaries_id,
+                                labels_id=labels_id, config=config)
 
                             # Compute evaluations
-                            results = msaf.eval.process(in_path, boundaries_id, labels_id,
-                                                ds_name, save=True, n_jobs=n_jobs,
-                                                config=config)
+                            results = msaf.eval.process(
+                                in_path, boundaries_id, labels_id,
+                                save=True, n_jobs=n_jobs, config=config)
 
                             # Save avg results
                             new_columns = {"config_M": M, "config_m": m,
                                            "config_k": k, "config_Mp": Mp,
                                            "config_ot": ot}
                             results = results.append([new_columns],
-                                                    ignore_index=True)
+                                                     ignore_index=True)
                             all_results = all_results.append(results.mean(),
-                                                            ignore_index=True)
+                                                             ignore_index=True)
                             all_results.to_csv(results_file)
 
     else:
@@ -110,8 +111,9 @@ def process(in_path, annot_beats=False, feature="mfcc", ds_name="*",
 
 def main():
     """Main function to sweep parameters of a certain algorithm."""
-    parser = argparse.ArgumentParser(description=
-        "Runs the speficied algorithm(s) on the MSAF formatted dataset.",
+    parser = argparse.ArgumentParser(
+        description="Runs the speficied algorithm(s) on the MSAF "
+        "formatted dataset.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("in_path",
                         action="store",
@@ -145,13 +147,7 @@ def main():
                         help="Label algorithm identifier",
                         dest="labels_id",
                         default=None,
-                        choices= io.get_all_label_algorithms())
-    parser.add_argument("-d",
-                        action="store",
-                        dest="ds_name",
-                        default="*",
-                        help="The prefix of the dataset to use "
-                        "(e.g. Isophonics, SALAMI")
+                        choices=io.get_all_label_algorithms())
     parser.add_argument("-j",
                         action="store",
                         dest="n_jobs",
@@ -163,9 +159,8 @@ def main():
 
     # Run the algorithm(s)
     process(args.in_path, annot_beats=args.annot_beats, feature=args.feature,
-            ds_name=args.ds_name, framesync=args.framesync,
-            boundaries_id=args.boundaries_id, labels_id=args.labels_id,
-            n_jobs=args.n_jobs)
+            framesync=args.framesync, boundaries_id=args.boundaries_id,
+            labels_id=args.labels_id, n_jobs=args.n_jobs)
 
     # Done!
     logging.info("Done! Took %.2f seconds." % (time.time() - start_time))
