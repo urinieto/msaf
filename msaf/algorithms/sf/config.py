d76c05fa8fab03409a8b9f1cb171bfb206ea5dc7
Oriol Nieto <oriol.nieto@gmail.com>
2016-10-10 20:52:57 -0700

    Forgot to import numpy for the config #51

--- msaf/algorithms/sf/config.py
+++ msaf/algorithms/sf/config.py
@@ -4,20 +4,20 @@ import numpy as np
 
 # Serra params
 config = {
-    "M_gaussian"    : 27,
-    "m_embedded"    : 3,
-    "k_nearest"     : 0.04,
-    "Mp_adaptive"   : 28,
-    "offset_thres"  : 0.05,
-    "norm_feats"    : np.inf  # min_max, log, np.inf,
-                              # -np.inf, float >= 0, None
+    "M_gaussian": 27,
+    "m_embedded": 3,
+    "k_nearest": 0.04,
+    "Mp_adaptive": 28,
+    "offset_thres": 0.05,
+    "norm_feats": np.inf  # min_max, log, np.inf,
+                          # -np.inf, float >= 0, None
 
     # For framesync features
-    #"M_gaussian"    : 100,
-    #"m_embedded"    : 3,
-    #"k_nearest"     : 0.06,
-    #"Mp_adaptive"   : 100,
-    #"offset_thres"  : 0.01
+    # "M_gaussian"    : 100,
+    # "m_embedded"    : 3,
+    # "k_nearest"     : 0.06,
+    # "Mp_adaptive"   : 100,
+    # "offset_thres"  : 0.01
 }
 
 algo_id = "sf"
