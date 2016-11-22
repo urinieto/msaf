307c09f88a7dfff94496b219f3c30ffa6ab45f3c
Cheng-i Wang <wangsix@gmail.com>
2016-11-22 14:41:54 -0800

    fixed bugs
    Former-commit-id: d38a6a1dbd623de05582592749c09b263e1a0257
    Former-commit-id: c3f2f1811139a50938fbf0dca6be3a245e3c1a56

--- msaf/algorithms/vmo/segmenter.py
+++ msaf/algorithms/vmo/segmenter.py
@@ -2,8 +2,6 @@
 Example of algorithm for MSAF
 """
 from msaf.algorithms.interface import SegmenterInterface
-import numpy as np
-from ..scluster.main import time_clusterer
 from vmo.analysis.segmentation import segmentation
 import vmo
 import librosa
@@ -23,13 +21,14 @@ class Segmenter(SegmenterInterface):
         F = self._preprocess()
 
         F = librosa.feature.stack_memory(F.T).T
+        print F.shape
         ideal_t = vmo.find_threshold(F, dim=F.shape[1])
-        oracle = vmo.build_oracle(F, threshold=ideal_t[0][1])
+        oracle = vmo.build_oracle(F,flag='a', threshold=ideal_t[0][1], dim=F.shape[1])
 
         my_bounds, my_labels = segmentation(oracle, method=self.config['method'],
                                             connectivity=self.config['connectivity'])
         # Post process estimations
         est_idxs, est_labels = self._postprocess(my_bounds, my_labels)
-
+        print est_idxs
         # We're done!
         return est_idxs, est_labels
