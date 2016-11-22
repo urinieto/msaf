b6f9690d456112e8a7c32e88f202ec211ecea39e
Cheng-i Wang <wangsix@gmail.com>
2016-11-22 14:05:13 -0800

    added vmo segmentation methods
    Former-commit-id: b9d52a9c2bdce43299c8fd3a062ae6d610d6678d
    Former-commit-id: f575e49446c8f0496623d6dfa0ad4a9077eccc16

---
+++ msaf/algorithms/vmo/segmenter.py
@@ -0,0 +1,35 @@
+"""
+Example of algorithm for MSAF
+"""
+from msaf.algorithms.interface import SegmenterInterface
+import numpy as np
+from ..scluster.main import time_clusterer
+from vmo.analysis.segmentation import segmentation
+import vmo
+import librosa
+
+
+class Segmenter(SegmenterInterface):
+    def processFlat(self):
+        """Main process.
+        Returns
+        -------
+        est_idxs : np.array(N)
+            Estimated indeces the segment boundaries in frame indeces.
+        est_labels : np.array(N-1)
+            Estimated labels for the segments.
+        """
+        # Preprocess to obtain features (array(n_frames, n_features))
+        F = self._preprocess()
+
+        F = librosa.feature.stack_memory(F.T).T
+        ideal_t = vmo.find_threshold(F, dim=F.shape[1])
+        oracle = vmo.build_oracle(F, threshold=ideal_t[0][1])
+
+        my_bounds, my_labels = segmentation(oracle, method=self.config['method'],
+                                            connectivity=self.config['connectivity'])
+        # Post process estimations
+        est_idxs, est_labels = self._postprocess(my_bounds, my_labels)
+
+        # We're done!
+        return est_idxs, est_labels
