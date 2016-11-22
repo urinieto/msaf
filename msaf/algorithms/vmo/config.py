b9d52a9c2bdce43299c8fd3a062ae6d610d6678d
Cheng-i Wang <wangsix@gmail.com>
2016-11-22 14:05:13 -0800

    added vmo segmentation methods

---
+++ msaf/algorithms/vmo/config.py
@@ -0,0 +1,11 @@
+"""Example of a configuration file for a new algorithm"""
+
+# Algorithm Params
+config = {
+    "method": 'symbol_spectral',
+    "connectivity": 'lrs'
+}
+
+algo_id = "vmo"  # Identifier of the algorithm
+is_boundary_type = True  # Whether the algorithm extracts boundaries
+is_label_type = True  # Whether the algorithm labels segments
