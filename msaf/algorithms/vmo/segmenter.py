b3f76a7416637a5002613a8bef868e1fd0410276
Cheng-i Wang <wangsix@gmail.com>
2016-12-01 11:58:54 -0800

    tiny changes
    Former-commit-id: 393fccb798d0dc73cc3e178a31996513e4f35174
    Former-commit-id: 33854920d6834feeef14c696e13bcf579bf02b22

--- msaf/algorithms/vmo/segmenter.py
+++ msaf/algorithms/vmo/segmenter.py
@@ -24,9 +24,10 @@ class Segmenter(SegmenterInterface):
         F = librosa.feature.stack_memory(F.T).T
         F = pre.normalize(F, axis=0)
         ideal_t = vmo.find_threshold(F, dim=F.shape[1])
-        oracle = vmo.build_oracle(F,flag='a', threshold=ideal_t[0][1], dim=F.shape[1])
+        oracle = vmo.build_oracle(F, flag='a', threshold=ideal_t[0][1], dim=F.shape[1])
 
-        my_bounds, my_labels = segmentation(oracle, method=self.config['method'],
+        my_bounds, my_labels = segmentation(oracle,
+                                            method=self.config['method'],
                                             connectivity=self.config['connectivity'])
         # Post process estimations
         est_idxs, est_labels = self._postprocess(my_bounds, my_labels)
