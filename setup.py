fe2e3360d95b909d59321e95c1379ac0715b0c64
Oriol Nieto <oriol.nieto@gmail.com>
2017-03-20 19:32:24 -0700

    Compatible with Python 3.6
    Former-commit-id: 3d890985ffcd4293885dde09ea52f32f99842cc8
    Former-commit-id: cf2b0654584bb4db8ec99391bbcf92ff8e6098e6

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
     keywords='audio music sound',
     license='MIT',
