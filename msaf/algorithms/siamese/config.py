4c28a727a8a726dc6c34526a68c86d67710f13d7
Oriol Nieto <oriol.nieto@gmail.com>
2017-03-20 21:38:42 -0700

    Initial commit for the siamese algorithm
    Former-commit-id: ff8eef1ec8d655779c5d424deacecf0a8ec20ecd
    Former-commit-id: 341e427aeca2b1114eda972fcd0f3a7951312bc8

---
+++ msaf/algorithms/siamese/config.py
@@ -0,0 +1,10 @@
+"""Siamese Network Similarity"""
+
+# Algorithm Params
+config = {
+    "my_param1": 1.0
+}
+
+algo_id = "siamese"  # Identifier of the algorithm
+is_boundary_type = False  # Whether the algorithm extracts boundaries
+is_label_type = True  # Whether the algorithm labels segments
