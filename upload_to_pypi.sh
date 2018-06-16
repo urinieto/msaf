a985f3957480d5b55c4d8ed9abb8db309c588ca9
Oriol Nieto <onieto@pandora.com>
2018-06-15 17:41:36 -0700

    Added python 2 and 3 to custom script to upload to pypi

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 python setup.py egg_info
-python setup.py sdist bdist_wheel
+python2 setup.py sdist bdist_wheel
+python3 setup.py sdist bdist_wheel
 twine upload dist/*
 cd docs
 make html
