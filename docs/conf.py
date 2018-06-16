29d8c53fdc0aa412bb40a2da648e71dd0260cced
Oriol Nieto <onieto@pandora.com>
2018-06-15 17:43:36 -0700

    updated copyright year
    Former-commit-id: 25e520837c63386517c14383bfa0261e58545579
    Former-commit-id: cd5cd378ee63248da37e84fc8589a8d3a6e08f03

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'msaf'
-copyright = u'2016, msaf development team'
+copyright = u'2015-2018, msaf development team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
