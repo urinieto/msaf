7db63867fb415575eca1ff7a1ece07751b709f9b
Carl Thomé <carl.thome@epidemicsound.com>
2023-11-22 14:15:29 +0100

    Move example segmenter out of source and into examples/

--- msaf/algorithms/__init__.py
+++ msaf/algorithms/__init__.py
@@ -1,9 +1,8 @@
 """Initialisation file for all the algorithms contained in MSAF."""
-from . import cnmf, example, fmc2d, foote, olda, scluster, sf, vmo
+from . import cnmf, fmc2d, foote, olda, scluster, sf, vmo
 
 __all__ = [
     "cnmf",
-    "example",
     "fmc2d",
     "foote",
     "olda",
